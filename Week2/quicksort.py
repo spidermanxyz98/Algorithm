8dbe6e9262a4ef6ced3fb7e876e0b95bdb322bed
Xinlin <xlsong98@gmail.com>
2016-10-23 16:29:25 -0400

    Change how to swap two element

--- Week2/quicksort.py
+++ Week2/quicksort.py
@@ -13,20 +13,19 @@ def partition(A):
     n=len(A)
     if n>=2:
         k=np.random.randint(0,n-1)  #choose the piviot
-        piviot=A[k]
-        A[k]=A[0]
-        A[0]=piviot
-        i=0
+        
+        #move the piviot to the first element
+        A[0], A[k] = A[k], A[0]
+        
+        i=0     #label the boundary
         for j in range(1,n):
             if A[0]>=A[j]:
-                temp=A[i+1]
-                A[i+1]=A[j]
-                A[j]=temp
+                A[i+1], A[j] = A[j], A[i+1]
                 i+=1
-        temp=A[0]
-        A[0]=A[i]
-        A[i]=temp
+        #move the pivoit element to the right position
+        A[i], A[0] = A[0], A[i]
     
+        #partition the list
         if i>0 and i<j:
             left=A[0:i+1]
             right=A[i+1:n]
@@ -53,12 +52,12 @@ def quicksort(A):
         return(A)
         
 ###Test the running time of quicksort
-array_sizes=list((10**exp for exp in range(8,9)))
+array_sizes=list((10**exp for exp in range(5,6)))
 run_time_home=[]
 #run_time_python=[]
 
 for array_size in array_sizes:  
-    test=np.random.randn(array_size)
+    test=np.random.random_integers(1, 10**7,array_size)
     start_time=time.time()
     result=quicksort(test)
     run_time_home.append(time.time()-start_time)
@@ -66,7 +65,6 @@ for array_size in array_sizes:
     #start_time=time.time()
     #result=np.sort(test)
     #run_time_python.append(time.time()-start_time)
-    #print(run_time)
     #print(array_sizes)
     #print('Time elapsed:',time.time()-start_time)
     #print(result[:10])
@@ -78,7 +76,7 @@ plt.xscale('log')
 #plt.yscale('log')
 
 
-scale=list(map(lambda x:x*np.log(x), list(array_sizes)))
-plt.figure(1)
-plt.plot(array_sizes,scale,'o')
-plt.xscale('log')
+#scale=list(map(lambda x:x*np.log(x), list(array_sizes)))
+#plt.figure(1)
+#plt.plot(array_sizes,scale,'o')
+#plt.xscale('log')
